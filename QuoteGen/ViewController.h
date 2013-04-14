32260f9f48a7386c6369270fced6bf74c5f2023d
Ben Wigley <benwigley2@gmail.com>
2013-04-14 19:55:56 +1200

    Initial Commit

---
+++ QuoteGen/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  QuoteGen
+//
+//  Created by Ben Wigley on 14/04/13.
+//  Copyright (c) 2013 Ben Wigley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ QuoteGen/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  QuoteGen
+//
+//  Created by Ben Wigley on 14/04/13.
+//  Copyright (c) 2013 Ben Wigley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
